509707dc1dbb6ac0cab8fbaa812de29d3a81266c
Xin Wang <xin1.wang@intel.com>
2026-05-28 18:34:33 +0800

    Fix property Signed-off-by: Xin Wang <xin1.wang@intel.com>
    Signed-off-by: Xin Wang <xin1.wang@intel.com>

--- src/plugins/intel_npu/src/plugin/src/properties.cpp
+++ src/plugins/intel_npu/src/plugin/src/properties.cpp
@@ -774,7 +774,6 @@ void Properties::registerCompiledModelProperties() {
     TRY_REGISTER_COMPILEDMODEL_PROPERTY_IFSET(ov::intel_npu::backend_compilation_params, BACKEND_COMPILATION_PARAMS);
     TRY_REGISTER_COMPILEDMODEL_PROPERTY_IFSET(ov::intel_npu::bypass_umd_caching, BYPASS_UMD_CACHING);
     TRY_REGISTER_COMPILEDMODEL_PROPERTY_IFSET(ov::intel_npu::defer_weights_load, DEFER_WEIGHTS_LOAD);
-    TRY_REGISTER_COMPILEDMODEL_PROPERTY_IFSET(ov::intel_npu::commandlist_mode, COMMANDLIST_MODE);
     TRY_REGISTER_COMPILEDMODEL_PROPERTY_IFSET(ov::intel_npu::compiler_dynamic_quantization,
                                               COMPILER_DYNAMIC_QUANTIZATION);
     TRY_REGISTER_COMPILEDMODEL_PROPERTY_IFSET(ov::intel_npu::qdq_optimization, QDQ_OPTIMIZATION);
@@ -790,7 +789,6 @@ void Properties::registerCompiledModelProperties() {
     TRY_REGISTER_COMPILEDMODEL_PROPERTY_IFSET(ov::intel_npu::enable_strides_for, ENABLE_STRIDES_FOR);
 
     TRY_REGISTER_VARPUB_PROPERTY(ov::intel_npu::batch_mode, BATCH_MODE, false);
-    TRY_REGISTER_VARPUB_PROPERTY(ov::intel_npu::commandlist_mode, COMMANDLIST_MODE, false);
     TRY_REGISTER_VARPUB_PROPERTY(ov::intel_npu::shared_common_queue, SHARED_COMMON_QUEUE, false);
 
     TRY_REGISTER_CUSTOM_PROPERTY(ov::hint::model_priority,
